d816f583cc5ca292c57d2310ff0e7df52b9cd119
michaeldobosh <muxa_dobosh@mail.ru>
2024-03-03 13:10:53 +0800

    upd

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ import path from 'node:path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import PugPlugin from 'pug-plugin';
 // import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -12,6 +13,9 @@ const mode = process.env.NODE_ENV || 'development';
 
 export default {
   mode,
+  // entry: {
+  //   index: './server/views/index.pug',
+  // },
   output: {
     path: path.join(__dirname, 'dist/'),
   },
@@ -19,7 +23,7 @@ export default {
     rules: [
       // {
       //   test: /\.pug$/,
-      //   loader: '@webdiscus/pug-loader',
+      //   loader: PugPlugin.loader,
       // },
       {
         test: /\.css$/,
@@ -35,9 +39,11 @@ export default {
       },
     ],
   },
-  plugins: [new MiniCssExtractPlugin(),
+  plugins: [
+    // new PugPlugin(),
+    new MiniCssExtractPlugin(),
     // new HtmlWebpackPlugin({
-    //   template: path.join(__dirname, './server/views/index.pug'),
+    //   template: path.join(__dirname, './server/views/layouts/application.pug'),
     //   filename: 'index.html',
     // }),
   ],
